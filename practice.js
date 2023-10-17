a95d59ae260598b443026058178cffdd88125fcc
최은철 <wndqhrgkdlel@gmail.com>
2023-10-17 16:49:00 +0900

    evel 함수 / 문자열을 변수로 인식시켜줌

---
+++ practice.js
@@ -0,0 +1,12 @@
+let i = 0;
+
+i = 5 ;
+
+let array1 = [[i, i+1, i+2, i+3], [i+5, i+6, i+7, i+8]] ;
+
+function arrayzz(x,y){
+    console.log(eval(`array${x}[${y}]`));
+}
+
+console.log(array1[1]);
+arrayzz(1,1);
