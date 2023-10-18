55f46ae5b8444182cc6dfff1e3c569834aa42634
최은철 <wndqhrgkdlel@gmail.com>
2023-10-18 09:17:45 +0900

    구조분해할당 작성법 연습

---
+++ develop-16.js
@@ -0,0 +1,14 @@
+const _pokemon = {
+    pokemonName: "피카츄",
+    type: "전기",
+    level: 42,
+    skells: ["10만볼트","전광석화", "아이언테일"]
+};
+
+console.log(_pokemon.pokemonName);
+console.log(_pokemon.type);
+
+const {pokemonName, type} = _pokemon;
+
+console.log(pokemonName);
+console.log(type);
